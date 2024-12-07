d0a2f146c88189c7639a821e1544ae9e6cc2c293
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2024-12-07 14:50:47 +0530

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import About from "./pages/About";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
-function App() {
+const App = () => {
   return (
     <>
       <Home />
       <About />
     </>
   );
-}
+};
 
 export default App;
