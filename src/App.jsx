625f454ffbc3f781b80493e46d87d29e14c77545
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2024-12-07 14:48:05 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 import About from "./pages/About";
 import Home from "./pages/home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Home />
