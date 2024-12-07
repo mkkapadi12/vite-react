404ffb98d379ea14907569650744b53702d7c45a
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2024-12-07 15:45:42 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import React from "react";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
+<<<<<<< Updated upstream
 import { Logout } from "./pages/Logout";
+=======
+import Login from "./pages/Login";
+>>>>>>> Stashed changes
 
 const App = () => {
   return (
     <>
       <Home />
+      <Login />
       <About />
+<<<<<<< Updated upstream
       <Contacts/>
       <Logout/>
+=======
+      <Contacts />
+>>>>>>> Stashed changes
     </>
   );
 };
